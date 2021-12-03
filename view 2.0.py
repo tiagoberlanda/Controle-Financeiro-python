46dc002279dc52d203faa532384928a20cd7e9ae
Tiago Berlanda <79882301+tiagoberlanda@users.noreply.github.com>
2021-12-03 14:07:36 -0300

    Update view 2.0.py

--- view 2.0.py
+++ view 2.0.py
@@ -1,17 +1,17 @@
 from tkinter import *
 from tkinter.ttk import Combobox
 import pymysql
-from datetime import datetime, timedelta
-import os
 
 class Janela:
     ##### Criando janela Princiapal #####
+    global app
     app = Tk()  # Criando janela
     app.geometry('600x450')  # Tamanho da janela
     app.title('Controle de Finanças')  # Título da Janela
     #app.resizable(width=False, height=False) # Trava para Não abrir tela cheia
 
     ##### Criando Informativo de Erros #####
+    global info
     info = StringVar()      #Definindo variavel com o valor que será exibido na tela
     info.set('Status : ')     #Setando o texto para aparecer na tela
     informacao_Bd = Label(app, textvariable=info,bd=1,relief="solid", font='Arial 12')
@@ -33,6 +33,7 @@ class Janela:
     descricao.grid(row=0, column=0)  # Posição no Grid
 
     # Text Box Descrição #
+    global descricao_Tb
     descricao_Tb = Entry(app)
     descricao_Tb.grid(row=0, column=1)  # Posição no Grid
 
@@ -51,6 +52,7 @@ class Janela:
     valor.grid(row=1, column=0)  # Posição no Grid
 
     ## Text Box Valor #
+    global valor_Tb
     valor_Tb = Entry(app)
     valor_Tb.grid(row=1, column=1)  # Posição no Grid
 
@@ -68,6 +70,7 @@ class Janela:
     categoria.grid(row=1, column=4)  # Posição no Grid
 
     # Combobox Categoria #
+    global categoria_cb
     categoria_cb = Combobox(app, values=["Remuneração", "Saúde", "Transporte", "Educação", "Cuiadados Pessoais"])
     categoria_cb.grid(row=1, column=5)  # Posição no Grid
 
@@ -85,6 +88,7 @@ class Janela:
     data.grid(row=0, column=4)  # Posição no Grid
 
     # Text Box Data #
+    global data_Tb
     data_Tb = Entry(app)
     data_Tb.grid(row=0, column=5)  # Posição no Grid
 
@@ -101,6 +105,7 @@ class Janela:
     tipo_lb.grid(row=0, column=6)  # Posição no Grid
 
     # Combobox Tipo #
+    global tipo_cb
     tipo = StringVar()
     tipo_cb = Combobox(app, values=["Receita", "Despesa"])
     tipo_cb.grid(row=1, column=6)  # Posição no Grid
@@ -126,13 +131,45 @@ class Janela:
             info.set('Status : Conectado com sucesso!')
         except:
             info.set('Status : Erro de conexão!')
+
+    # Criando novo registro no banco
+    def novoRegistro():
+        conteudo_descricao = str(descricao_Tb.get())
+        conteudo_valor = str(valor_Tb.get())
+        conteudo_categoria = str(categoria_cb.get())
+        conteudo_data = str(data_Tb.get())
+        conteudo_tipo = str(tipo_cb.get())
+
+        # Validação de dados, se tem ou não valor em todos os campos
+        if conteudo_descricao == "" or conteudo_valor == "" or conteudo_categoria == "" or conteudo_data == "" or conteudo_tipo == "":
+            info.set('Status : Preencha todas as informações!')
+        else:
+            # Tentativa de conexão com o banco
+            try:
+                con = pymysql.Connect(host='localhost', database='controlefinanceiro', user='root', password='')
+                cursor = con.cursor()
+                cursor.execute(f"INSERT INTO financas(nomeFinanca, valorFinanca, tipoFinanca, dataFinanca, categoriaFinanca)"
+                               f" VALUES ('{conteudo_descricao}','{conteudo_valor}','{conteudo_tipo}','{conteudo_data}','{conteudo_categoria}')")
+                con.commit()    #Salva as alterações
+                info.set('Status : Dados inseridos com Sucesso!')   #Mensagem de sucesso na inserção dos dados
+
+            except Exception as e:
+                info.set(f'Erro: {e}')
+
+            #Limpar dados após inserir na Base
+            descricao_Tb.delete(0,'end')
+            valor_Tb.delete(0,'end')
+            categoria_cb.delete(0,'end')
+            data_Tb.delete(0,'end')
+            tipo_cb.delete(0,'end')
+
     #### Criando Botões ####
     # Botão de Conexão
     Bt_conexao = Button(app, text='Conexão', command=conectaBanco)
     Bt_conexao.place(bordermode=OUTSIDE, height=40, width=60, x=60, y=80)  # Posição no Grid XY
 
     # Botão de Novo
-    Bt_novo = Button(app, text='Novo', command='')
+    Bt_novo = Button(app, text='Novo', command=novoRegistro)
     Bt_novo.place(bordermode=OUTSIDE, height=40, width=60, x=150, y=80)  # Posição no Grid XY
 
     # Botão de Editar
