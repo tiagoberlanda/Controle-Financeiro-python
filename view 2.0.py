e49ed809f6e576f50238acc667eb24090bc21730
Tiago Berlanda <79882301+tiagoberlanda@users.noreply.github.com>
2021-12-05 19:50:40 -0300

    Update view 2.0.py

--- view 2.0.py
+++ view 2.0.py
@@ -2,20 +2,20 @@ from tkinter import *
 from tkinter import ttk
 import pymysql
 
-class Janela:
+class Janela():
     ##### Criando janela Princiapal #####
     global app
     app = Tk()  # Criando janela
-    app.geometry('600x450')  # Tamanho da janela
+    app.geometry('650x450')  # Tamanho da janela
     app.title('Controle de Finanças')  # Título da Janela
-    #app.resizable(width=False, height=False) # Trava para Não abrir tela cheia
+    # app.resizable(width=False, height=False) # Trava para Não abrir tela cheia
 
-    ##### Criando Informativo de Erros #####
+    ### Criando Informativo de Erros #####
     global info
-    info = StringVar()      #Definindo variavel com o valor que será exibido na tela
-    info.set('Status : Não Conectado!')     #Setando o texto para aparecer na tela
-    informacao_Bd = Label(app, textvariable=info,bd=1,relief="solid", font='Arial 12')
-    informacao_Bd.place(bordermode=OUTSIDE, height=35, width=550, x=30, y=150)
+    info = StringVar()  # Definindo variavel com o valor que será exibido na tela
+    info.set('Status : Não Conectado!')  # Setando o texto para aparecer na tela
+    informacao_Bd = Label(app, textvariable=info, bd=1, relief="solid", font='Arial 12')
+    informacao_Bd.place(bordermode=OUTSIDE, height=35, width=600, x=30, y=150)
 
     ### Criando os Elementos da Tela ####
     # Label Descrição #
@@ -37,7 +37,7 @@ class Janela:
     descricao_Tb = Entry(app)
     descricao_Tb.grid(row=0, column=1)  # Posição no Grid
 
-    #Label Valor #
+    # Label Valor #
     valor = Label(
         app,
         text='Valor',
@@ -48,7 +48,7 @@ class Janela:
         pady=5,
         justify=LEFT,
         anchor=W
-        )
+    )
     valor.grid(row=1, column=0)  # Posição no Grid
 
     ## Text Box Valor #
@@ -110,7 +110,7 @@ class Janela:
     tipo_cb = ttk.Combobox(app, values=["Receita", "Despesa"])
     tipo_cb.grid(row=1, column=6)  # Posição no Grid
 
-    # Label Data
+    # Label Tipo
     data = Label(app,
                  text='Tipo',
                  bd=0,
@@ -123,21 +123,38 @@ class Janela:
                  )
     data.grid(row=0, column=6)  # Posição no Grid
 
+    # Label Id #
+    tipo_lb = Label(app,
+                    text='ID',
+                    bd=0,
+                    font="Arial 12",
+                    relief="solid",
+                    padx=5,
+                    pady=5,
+                    justify=CENTER,
+                    anchor=S, )
+    tipo_lb.grid(row=0, column=7)  # Posição no Grid
 
-    # Conectando com o Banco de dados #
+    ## Text Box ID #
+    global id_Tb
+    id_Tb = Entry(app,width=5)
+    id_Tb.grid(row=0,column=8)  # Posição no Grid
+
+    # Conectando com o Banco de dados e preenchendo treeview #
     def conectaBanco():
         try:
-            con = pymysql.connect(user='root',password='',database='controlefinanceiro',host='localhost')
+            con = pymysql.connect(user='root',database='controlefinanceiro',password='',host='localhost')
             cursor = con.cursor()
             info.set('Status : Conectado com sucesso!')
-        except:
-            info.set('Status : Erro de conexão!')
+        except Exception as e:
+            info.set(f'Status : Erro {e}')
 
-        #Puxar informações para o TreeView
-        query =cursor.execute('select * from financas')
+        # Puxar informações para o TreeView
+        query = cursor.execute('select * from financas')
         query2 = cursor.fetchall()
-        for i in range(0,query):
-            tv.insert("","end",values=query2)
+        for i in range(0, query):
+            tv.insert("", "end", values=query2[i])
+
     # Criando novo registro no banco
     def novoRegistro():
         conteudo_descricao = str(descricao_Tb.get())
@@ -154,20 +171,63 @@ class Janela:
             try:
                 con = pymysql.Connect(host='localhost', database='controlefinanceiro', user='root', password='')
                 cursor = con.cursor()
-                cursor.execute(f"INSERT INTO financas(nomeFinanca, valorFinanca, tipoFinanca, dataFinanca, categoriaFinanca)"
-                               f" VALUES ('{conteudo_descricao}','{conteudo_valor}','{conteudo_tipo}','{conteudo_data}','{conteudo_categoria}')")
-                con.commit()    #Salva as alterações
-                info.set('Status : Dados inseridos com Sucesso!')   #Mensagem de sucesso na inserção dos dados
+                cursor.execute(
+                    f"INSERT INTO financas(nomeFinanca, valorFinanca, tipoFinanca, dataFinanca, categoriaFinanca)"
+                    f" VALUES ('{conteudo_descricao}','{conteudo_valor}','{conteudo_tipo}','{conteudo_data}','{conteudo_categoria}')")
+                con.commit()  # Salva as alterações
+                info.set('Status : Dados inseridos com Sucesso!')  # Mensagem de sucesso na inserção dos dados
 
             except Exception as e:
                 info.set(f'Erro: {e}')
 
-            #Limpar dados após inserir na Base
-            descricao_Tb.delete(0,'end')
-            valor_Tb.delete(0,'end')
-            categoria_cb.delete(0,'end')
-            data_Tb.delete(0,'end')
-            tipo_cb.delete(0,'end')
+            # Limpar dados após inserir na Base
+            descricao_Tb.delete(0, 'end')
+            valor_Tb.delete(0, 'end')
+            categoria_cb.delete(0, 'end')
+            data_Tb.delete(0, 'end')
+            tipo_cb.delete(0, 'end')
+
+    # Excluir um registro do banco de dados #
+    def excluiRegistro():
+        try:
+            con = pymysql.Connect(host='localhost', database='controlefinanceiro', user='root', password='')
+            cursor = con.cursor()
+            id = id_Tb.get()
+            cursor.execute(f'DELETE FROM `financas` WHERE idFinanca = {id}')
+            con.commit()  # Salva as alterações
+            info.set('Status : Dados inseridos com Sucesso!')  # Mensagem de sucesso na inserção dos dados
+
+        except Exception as e:
+            info.set(f'Erro: {e}')
+
+    #Alterando registro do Banco de Dados
+    def alteraRegistro():
+        try:
+            con = pymysql.Connect(host='localhost', database='controlefinanceiro', user='root', password='')
+            cursor = con.cursor()
+            conteudo_descricao = str(descricao_Tb.get())
+            conteudo_valor = str(valor_Tb.get())
+            conteudo_categoria = str(categoria_cb.get())
+            conteudo_data = str(data_Tb.get())
+            conteudo_tipo = str(tipo_cb.get())
+            conteudo_id = int(id_Tb.get())
+            cursor.execute(f"UPDATE financas SET `nomeFinanca`='{conteudo_descricao}',`valorFinanca`='{conteudo_valor}'"
+                           f",`tipoFinanca`='{conteudo_tipo}',`categoriaFinanca`='{conteudo_categoria}',"
+                           f"`dataFinanca`='{conteudo_data}', WHERE idFinanca = '{conteudo_id}'")
+            con.commit()
+            info.set('Status : Dados inseridos com Sucesso!')  # Mensagem de sucesso na inserção dos dados
+
+        except Exception as e:
+            info.set(f'Erro: {e}')
+    def editaRegistro():
+        try:
+            con = pymysql.Connect(host='localhost', database='controlefinanceiro', user='root', password='')
+            cursor = con.cursor()
+            con.commit()  # Salva as alterações
+            info.set('Status : Dados inseridos com Sucesso!')  # Mensagem de sucesso na inserção dos dados
+
+        except Exception as e:
+            info.set(f'Erro: {e}')
 
     #### Criando Botões ####
     # Botão de Conexão
@@ -176,36 +236,37 @@ class Janela:
 
     # Botão de Novo
     Bt_novo = Button(app, text='Novo', command=novoRegistro)
-    Bt_novo.place(bordermode=OUTSIDE, height=40, width=60, x=150, y=80)  # Posição no Grid XY
+    Bt_novo.place(bordermode=OUTSIDE, height=40, width=60, x=180, y=80)  # Posição no Grid XY
 
     # Botão de Editar
-    Bt_editar = Button(app, text='Editar', command='')
-    Bt_editar.place(bordermode=OUTSIDE, height=40, width=60, x=240, y=80)  # Posição no Grid XY
+    Bt_editar = Button(app, text='Editar', command=editaRegistro)
+    Bt_editar.place(bordermode=OUTSIDE, height=40, width=60, x=300, y=80)  # Posição no Grid XY
 
     # Botão de Excluir
-    Bt_excluir = Button(app, text='Excluir', command='')
-    Bt_excluir.place(bordermode=OUTSIDE, height=40, width=60, x=330, y=80)  # Posição no Grid XY
+    Bt_excluir = Button(app, text='Excluir', command=excluiRegistro)
+    Bt_excluir.place(bordermode=OUTSIDE, height=40, width=60, x=420, y=80)  # Posição no Grid XY
 
     # Botão de Atualizar
-    Bt_atualizar = Button(app, text='Atualizar', command='')
-    Bt_atualizar.place(bordermode=OUTSIDE, height=40, width=60, x=420, y=80)  # Posição no Grid XY
+    Bt_atualizar = Button(app, text='Atualizar', command=alteraRegistro)
+    Bt_atualizar.place(bordermode=OUTSIDE, height=40, width=60, x=540, y=80)  # Posição no Grid XY
 
     # TreeViewer de informações
     global tv
-    tv = ttk.Treeview(app, columns=('id', 'nome', 'valor','categoria','data','tipo'), show='headings')
-    tv.place(bordermode=OUTSIDE, height=200, width=550, x=30, y=190)
-    tv.column('id',  anchor=CENTER, width=10)
+    tv = ttk.Treeview(app, columns=('id', 'nome', 'valor', 'tipo', 'categoria', 'data'), show='headings')
+    tv.place(bordermode=OUTSIDE, height=200, width=600, x=30, y=190)
+    tv.column('id', anchor=CENTER, width=10)
     tv.column('nome', anchor=CENTER, width=80)
     tv.column('valor', anchor=CENTER, width=80)
-    tv.column('categoria',anchor=CENTER,width=80)
-    tv.column('data',anchor=CENTER,width=80)
-    tv.column('tipo',anchor=CENTER,width=80)
+    tv.column('categoria', anchor=CENTER, width=80)
+    tv.column('data', anchor=CENTER, width=80)
+    tv.column('tipo', anchor=CENTER, width=80)
 
     tv.heading('id', text='Id', anchor=CENTER)
     tv.heading('nome', text='Nome', anchor=CENTER)
     tv.heading('valor', text='Valor', anchor=CENTER)
-    tv.heading('categoria',text = 'Categoria',anchor=CENTER)
-    tv.heading('data',text='Data',anchor=CENTER)
-    tv.heading('tipo',text = 'Tipo',anchor=CENTER)
+    tv.heading('categoria', text='Categoria', anchor=CENTER)
+    tv.heading('data', text='Data', anchor=CENTER)
+    tv.heading('tipo', text='Tipo', anchor=CENTER)
 
-    app.mainloop() #Mantem a janela aberta
+    app.mainloop()  # Mantem a janela aberta
+  
